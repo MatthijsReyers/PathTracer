16bc1eb39ad363e13bdc0bcfec6f9501178f61f1
MatthijsReyers <matthijsreyers@gmail.com>
2019-07-24 16:27:21 +0200

    First commit

---
+++ code/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+#include <math.h>
+#include <stdlib.h>
+
+int main(int argv, char** argc)
+{
+    const char* OutFileName = "/images/out.ppm";
+    std::ofstream OutFile;
+    OutFile.open(OutFileName);
+
+    int nx = 400;
+    int ny = 400;
+    OutFile << "P3\n" << nx << " " << ny << "\n255\n";
+
+    for (int h = ny-1; h >= 0; h--)
+    {
+        for (int w = 0; w < nx; w++)
+        {
+            float r = float(h) / float(ny);
+            float g = float(h) / float(ny);
+            float b = float(h) / float(ny);
+
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+
+            OutFile << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+    OutFile.close();
+}
